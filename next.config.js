90cc3778e96bca07e3ac81f5e5b93672835096ab
SKYE <skye@MacBook-Pro-MacBook.local>
2024-01-22 16:36:43 +0300

    Added analyzer

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 const path = require("path");
 
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   sassOptions: {
